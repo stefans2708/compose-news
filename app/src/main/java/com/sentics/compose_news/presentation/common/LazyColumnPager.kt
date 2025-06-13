9d78154f210b99bc728a6bb802e51127880e937f
Stefan <stefan.sentic@htecgroup.com>
2025-06-13 17:04:44 +0200

    [Custom Paging] 3 - create LazyColumnPager composable

---
+++ app/src/main/java/com/sentics/compose_news/presentation/common/LazyColumnPager.kt
@@ -0,0 +1,61 @@
+package com.sentics.compose_news.presentation.common
+
+import androidx.compose.foundation.OverscrollEffect
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberOverscrollEffect
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LazyColumnPager(
+    modifier: Modifier = Modifier,
+    state: LazyListState = rememberLazyListState(),
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    reverseLayout: Boolean = false,
+    verticalArrangement: Arrangement.Vertical = if (!reverseLayout) Arrangement.Top else Arrangement.Bottom,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    flingBehavior: FlingBehavior = ScrollableDefaults.flingBehavior(),
+    userScrollEnabled: Boolean = true,
+    overscrollEffect: OverscrollEffect? = rememberOverscrollEffect(),
+    prefetchDistance: Int = 10,
+    loadMore: () -> Unit,
+    content: LazyListScope.() -> Unit,
+) {
+    val shouldLoadMore by remember {
+        derivedStateOf {
+            state.layoutInfo.visibleItemsInfo.lastOrNull()?.index?.let { lastVisible ->
+                lastVisible > state.layoutInfo.totalItemsCount - prefetchDistance
+            } == true
+        }
+    }
+
+    LazyColumn(
+        modifier = modifier,
+        state = state,
+        contentPadding = contentPadding,
+        reverseLayout = reverseLayout,
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = horizontalAlignment,
+        flingBehavior = flingBehavior,
+        userScrollEnabled = userScrollEnabled,
+        overscrollEffect = overscrollEffect,
+        content = content
+    )
+
+    LaunchedEffect(shouldLoadMore) {
+        if (shouldLoadMore) loadMore()
+    }
+}
